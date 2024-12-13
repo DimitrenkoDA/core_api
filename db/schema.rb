ab773c6b564247db1e1e9e7a6a23549fc133e541
Dmitry Dimitrenko <53081747+DimitrenkoDA@users.noreply.github.com>
2024-12-14 00:12:20 +0100

    DB changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_30_150542) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_13_220604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_30_150542) do
     t.index ["step"], name: "index_authentications_on_step"
   end
 
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "polarity", null: false
+    t.integer "amount_cents", default: 0, null: false
+    t.string "amount_currency", default: "EUR", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest", null: false
@@ -40,4 +50,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_30_150542) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "transactions", "users"
 end
