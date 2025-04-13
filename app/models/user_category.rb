671c7b373933cac55f82114e4557344970ae09bf
dimitrenkoda <dimitrenko.da@gmail.com>
2025-04-13 20:55:19 +0200

    [DEV-27] Remove uniqness for categories name

--- app/models/user_category.rb
+++ app/models/user_category.rb
@@ -2,7 +2,7 @@ class UserCategory < ApplicationRecord
   belongs_to :user
 
   validates :kind, presence: true
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   validates :colour_code, presence: true, format: { with: /\A#[0-9a-f]{6}\z/i }
 
   enum :kind, {
